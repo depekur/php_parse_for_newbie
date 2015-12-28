4e79dff86cc490af9c153b96a01b5d67a8cd403b
depekur <depekur@gmail.com>
2015-12-28 04:30:18 +0300

    init

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Theatre</title>
+
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+
+
+</head>
+<body>
+
+<main class="scene">
+  <div class="actor">
+    <div class="actor__prefix">-</div>
+    <div id="vader" class="actor__content"></div>
+  </div>
+
+  <div class="actor">
+    <div class="actor__prefix">-</div>
+    <div id="luke" class="actor__content"></div>
+  </div>
+
+</main>
+
+<script type="text/javascript" src="js/theater.min.js"></script>
+<script type="text/javascript" src="js/theater.ru.min.js"></script>	
+<script type="text/javascript" src="js/app.js"></script>
+	
+</body>
+</html>
